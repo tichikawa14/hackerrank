e22866e8106400c06f526ea9d9c83cb4eef36090
tichikawa14 <gakusei.umeeee@gmail.com>
2019-11-06 16:27:32 +0900

    02_very_big_sum.rb

---
+++ very_big_sum.rb
@@ -0,0 +1,20 @@
+require 'json'
+require 'stringio'
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar)
+  ar.sum
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+ar_count = gets.to_i
+
+ar = gets.rstrip.split(' ').map(&:to_i)
+
+result = aVeryBigSum ar
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
